2cc985030db0c60b5fe327f2af7b7d3736ff69af
Aibatyr <aibatyr111@gmail.com>
2024-10-02 16:29:28 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController2.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController2 {
+    @GetMapping("/hello2")
+    public boolean fdfdfd() {
+        return true;
+    }
+}
